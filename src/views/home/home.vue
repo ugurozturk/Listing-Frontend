e05ed6b14c9efde7b518b78e2fbfd03cacb06dd1
Uğur Öztürk <mail@uozturk.com>
2019-09-12 21:54:34 +0300

    made home extend example

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,12 +1,25 @@
 <template>
-    <div>
-        <Card dis-hover></Card>
-    </div>
+<div>
+  <Card dis-hover>
+    <button @click="plus">{{ sayi }}</button>
+  </Card>
+</div>
 </template>
+
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  
+  data: function () {
+    return {
+      test: '',
+      sayi: 0
+    }
+  },
+  methods: {
+    plus: function () {
+      let Self = this
+      Self.sayi++
+    }
+  }
 })
 </script>
-
