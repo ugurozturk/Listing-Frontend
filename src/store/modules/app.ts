bf190f9e3269b9cbae9ca0d8ba95bcb12cd0c22c
Uğur Öztürk <mail@uozturk.com>
2020-03-29 11:05:25 +0300

    eslint mini fix

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -194,7 +194,7 @@ class AppModule implements Module<AppState, any>{
     }
     actions = {
         async login(content: ActionContext<AppState, any>, payload: any) {
-            let rep = await ajax.post("/api/TokenAuth/Authenticate", payload.data);
+            let rep = await ajax.post('/api/TokenAuth/Authenticate', payload.data);
             var tokenExpireDate = payload.data.rememberMe ? (new Date(new Date().getTime() + 1000 * rep.data.result.expireInSeconds)) : undefined;
             Util.abp.auth.setToken(rep.data.result.accessToken, tokenExpireDate);
             Util.abp.utils.setCookieValue(appconst.authorization.encrptedAuthTokenName, rep.data.result.encryptedAccessToken, tokenExpireDate, Util.abp.appPath)
